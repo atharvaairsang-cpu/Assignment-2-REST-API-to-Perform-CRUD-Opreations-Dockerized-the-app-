209702c06ba6849d6f277c0a9ac0958ff18fa0fc
atharvaairsang-cpu <atharva.airsang@iauro.com>
2026-09-10 16:39:13 +0530

    Delete Flask directory

--- Flask/database.py
+++
@@ -1,3 +0,0 @@
-from sqlalchemy import create_engine
-
-engine = create_engine('mysql+pymysql://root:demopassword@mysql:3307/demodb')
--- Flask/delete.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import text
-from database import engine
-
-
-def delete_user(id):
-    with engine.begin() as connection:
-        connection.execute(text("DELETE FROM users WHERE id = :id"), {"id": id})
